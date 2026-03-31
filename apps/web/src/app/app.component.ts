04a36699cb2eb0889c4270da4fae4d6f890562d7
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2026-03-31 20:04:33 +0700

    feat: remove create-turbo sample files

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyCounterButtonComponent } from '@repo/ui';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyCounterButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
